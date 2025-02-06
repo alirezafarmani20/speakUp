aea639bcaea78f601b9848b54b21e3df266d50d7
alireza <farmanialireza668@gmail.com>
2025-02-06 18:55:10 +0330

    install prettier and confing it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   modules: [
     // ...
-    '@pinia/nuxt',
+    "@pinia/nuxt",
   ],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  }
-})
+    plugins: [tailwindcss()],
+  },
+});
