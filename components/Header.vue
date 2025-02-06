e53c3f22df51ecb4735e3b0dc2f660d910b2f3ca
alireza <farmanialireza668@gmail.com>
2025-02-06 19:14:49 +0330

    create main layout

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import Container from '~/layouts/Container.vue';
+</script>
+
+<template>
+    <header class="w-full">
+        <Container>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                    <!--logo section-->
+                    <NuxtLink to="/">
+                        <img src="" alt="site logo">
+                    </NuxtLink>
+                </div>
+                <div class="">
+                    <!--menu section-->
+                </div>
+                <div class="">
+                    <!--register box-->
+                </div>
+            </div>
+        </Container>
+    </header>
+</template>
+
+
+<style scoped></style>
---
+++ layouts/Container.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="mx-4 sm:mx-6">
+        <slot></slot>
+    </div>
+</template>
